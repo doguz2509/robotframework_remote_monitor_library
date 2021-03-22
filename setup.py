d4d773c8eb39d11efa62b5e5b1e190f5c28ad4a5
doguz2509 <vWMA25Y6Vmc4UJSW4vUB>
2021-03-22 18:53:08 +0200

    adding package files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='robotframework_system_trace_collector',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Dmitry Oguz',
+    author_email='doguz2509@gmail.com',
+    description='PlugIn for robotframework allow background system tracing'
+)
