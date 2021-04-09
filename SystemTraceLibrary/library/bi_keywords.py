245c0e72ad17e3e7a6d3169e13e031b415f5f591
doguz2509 <vWMA25Y6Vmc4UJSW4vUB>
2021-04-09 17:27:35 +0300

    chart model re structure

--- SystemTraceLibrary/library/bi_keywords.py
+++ SystemTraceLibrary/library/bi_keywords.py
@@ -8,6 +8,7 @@ from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
 
 from SystemTraceLibrary.api import db
 from SystemTraceLibrary.api.db import TableSchemaService
+from SystemTraceLibrary.model.chart_model import generate_charts
 from SystemTraceLibrary.model.chart_model.html_template import HTML_IMAGE_REF, HTML
 from SystemTraceLibrary.model.host_registry_model import HostModule, HostRegistryCache
 from SystemTraceLibrary.utils.sql_engine import DB_DATETIME_FORMAT
@@ -37,20 +38,23 @@ class BIKeywords:
         self._log_path = rel_log_path
         self._images = images
         self._image_path = os.path.normpath(os.path.join(self._output_dir, self._log_path, self._images))
+        if not os.path.exists(self._image_path):
+            os.mkdir(self._image_path)
 
     def get_keyword_names(self):
         return [self.generate_module_statistics.__name__]
+
+    @staticmethod
+    def _create_chart_title(period, plugin, alias, delimiter='->'):
+        return delimiter.join([name for name in [period, plugin, alias] if name is not None])
 
     @keyword("Generate Module Statistics")
     def generate_module_statistics(self, period=None, plugin=None, alias=None):
         module: HostModule = HostRegistryCache().get_connection(alias)
         marks = _get_period_marks(period, module.host_id) if period else {}
-
-        if not os.path.exists(self._image_path):
-            os.mkdir(self._image_path)
-        body = ''
-
         chart_plugins = {k: v for k, v in module.active_plugins.items() if plugin is None or type(v).__name__ == plugin}
+        chart_title = self._create_chart_title(period, plugin, module.alias)
+        body = ''
 
         for alias, plugin in chart_plugins.items():
             for chart in plugin.affiliated_charts():
@@ -58,19 +62,20 @@ class BIKeywords:
                     sql_query = chart.compose_sql_query(host_name=plugin.thread_name, **marks)
                     logger.debug(f"{plugin.type}{f'_{period}' if period else ''}_{marks}\n{sql_query}")
                     sql_data = db.DataHandlerService().execute(sql_query)
-                    for picture_name, file_path in chart.generate(sql_data, self._image_path,
-                                                                  prefix=f"{plugin.type}{f'_{period}' if period else ''}"):
+                    prefix = f"{plugin.type}{f'_{period}' if period else ''}"
+                    for picture_name, file_path in generate_charts(chart, sql_data, self._image_path, prefix=prefix):
                         relative_image_path = os.path.relpath(file_path, os.path.normpath(
                             os.path.join(self._output_dir, self._log_path)))
                         body += HTML_IMAGE_REF.format(relative_path=relative_image_path, picture_title=picture_name)
                 except Exception as e:
                     logger.error(f"Error: {e}")
 
-        html_file_name = "{}.html".format(re.sub(r'\s+|@|:', '_', period or module.alias))
+        html_file_name = "{}.html".format(re.sub(r'\s+|@|:|>', '_', chart_title).replace('__', '_'))
         html = HTML.format(title=period or module.alias, body=body)
         html_full_path = os.path.normpath(os.path.join(self._output_dir, self._log_path, html_file_name))
         html_link_path = '/'.join([self._log_path, html_file_name])
         with open(html_full_path, 'w') as sw:
             sw.write(html)
-        logger.warn(f"<a href=\"{html_link_path}\">Session '{period or module.alias}' statistics</a>", html=True)
-        return f"<a href=\"{html_link_path}\">Session '{period or module.alias}' statistics</a>"
+        html_link_text = f"<a href=\"{html_link_path}\">Chart for '{chart_title}'</a>"
+        logger.warn(html_link_text, html=True)
+        return html_link_text
