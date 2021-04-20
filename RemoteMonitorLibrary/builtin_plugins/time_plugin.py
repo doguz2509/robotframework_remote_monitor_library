fdb0365adb06c18ad71f097de4b17a46d8d84d8e
doguz2509 <vWMA25Y6Vmc4UJSW4vUB>
2021-04-20 15:37:32 +0300

    time_plugin.py: consolidate plugin within one file

--- RemoteMonitorLibrary/builtin_plugins/time_plugin.py
+++ RemoteMonitorLibrary/builtin_plugins/time_plugin.py
@@ -12,7 +12,7 @@ and you'll know something is happening when you start to see real compilation ha
 2021-03-25T18:58:43+02:00:	679.22 real,	1082.81 user,	160.85 sys,	248824 max_mem_kb,	4 page_faults,	120697 involuntarily_ctx_swtc,	98104 file_inputs,	1492752 file_outputs,	0 socket_recieved,	0 socket_sent
 #BTW the above was without mlp ... so let's make sure we run at least 10 samples without mlp and 10 samples after - to conclude on avg, max and min for without mlp and then for with mlp """
 
-from typing import Iterable
+from typing import Iterable, Any, Tuple
 
 from SSHLibrary import SSHLibrary
 from robot.api import logger
@@ -20,11 +20,149 @@ from robot.utils import DotDict
 
 from RemoteMonitorLibrary.api import plugins, model, db
 from RemoteMonitorLibrary.api.tools import Logger
-from .charts import TimeChart
-from .tables import TimeMeasurement, CMD_TIME_FORMAT
-from ...utils import get_error_info
+
+
+__doc__ = """
+== Time plugin overview ==
+    Wrap linux /usr/bin/time utility for periodical execution and monitor of customer command for process io, memory, cpu, etc
+
+    Full documentation for time utility available on [https://linux.die.net/man/1/time|time man(1)]
+
+    === Plugin parameters ===
+
+    Parameters can be supplied via keyword `Start monitor plugin` as key-value pairs
+
+    Time plugin arguments:
+
+    - command: command to be executed and measured by time (Mandatory)
+
+    | /usr/bin/time -f "%... all time format fields (see man)" 'command' > /dev/null
+
+      Note: Pay attention not to redirect command stderr to stdout (avoid '2>&1'); Time write to stderr by itself and it send to parser
+
+    - name: User friendly alias for command (Optional)
+    - start_in_folder: path to executable binary/script if execution by path not relevant (Optional)
+
+      If provided command will be executed as following:
+
+    | cd 'start_in_folder' ; /usr/bin/time -f "" 'command' > /dev/null
+
+    - sudo: True if sudo required, False if omitted (Optional)
+    - sudo_password: True if password required for sudo, False if omitted (Optional)
+
+      On plugin start sudo and sudo_password will be replace with sudo password provided for connection module
+
+"""
+
+from RemoteMonitorLibrary.utils import get_error_info
 
 DEFAULT_TIME_COMMAND = r'/usr/bin/time'
+
+
+CMD_TIME_FORMAT = DotDict(
+    TimeReal="e",
+    TimeKernel="S",
+    TimeUser="U",
+    TimeCPU="P",
+    MemoryMaxResidentSize="M",
+    MemoryAverage="t",
+    MemoryAverageTotal="K",
+    MemoryAverageProcessData="D",
+    MemoryAverageProcessStack="p",
+    MemoryAverageProcessShared="X",
+    MemorySystemPageSize="Z",
+    MemoryMajorPageFaults="F",
+    MemoryMinorPageFaults="R",
+    MemoryProcessSwapped="W",
+    MemoryProcessContextSwitched="c",
+    MemoryWait="w",
+    IOInput="I",
+    IOOutput="O",
+    IOSocketRecieved="r",
+    IOSocketSent="s",
+    IOSignals="k",
+    Rc="x",
+    Command="C"
+)
+
+
+class TimeMeasurement(model.TimeReferencedTable):
+    def __init__(self):
+        model.TimeReferencedTable.__init__(self,
+                                           name='TimeMeasurement',
+                                           fields=[model.Field(f, model.FieldType.Int)
+                                                   for f in CMD_TIME_FORMAT.keys()])
+        # +
+        # [model.Field('OUTPUT_ID')])
+
+
+class LinesCacheMap(model.Table):
+    def __init__(self):
+        model.Table.__init__(self, name=None,
+                             fields=[
+                                 model.Field('OUTPUT_REF', model.FieldType.Int),
+                                 model.Field('ORDER_ID', model.FieldType.Int),
+                                 model.Field('LINE_REF', model.FieldType.Int)],
+                             foreign_keys=[model.ForeignKey('OUTPUT_REF', 'TimeMeasurement', 'OUTPUT_ID'),
+                                           model.ForeignKey('LINE_REF', 'LinesCache', 'LINE_ID')],
+                             queries=[model.Query('last_output_id', 'select max(OUTPUT_REF) from LinesCacheMap')])
+
+
+class LinesCache(model.Table):
+    def __init__(self):
+        model.Table.__init__(self, name=None,
+                             fields=[model.Field('LINE_ID',
+                                                 model.FieldType.Int,
+                                                 model.PrimaryKeys(True)),
+                                     model.Field('Line')])
+
+
+class TimeChart(plugins.ChartAbstract):
+    def __init__(self, table: model.Table, title, *sections):
+        self._table = table
+        plugins.ChartAbstract.__init__(self, *(sections if len(sections) > 0 else self._table.columns))
+        self._title = title
+
+    @property
+    def sections(self):
+        return self._sections
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def file_name(self) -> str:
+        return "{name}.png"
+
+    @property
+    def get_sql_query(self) -> str:
+        return """
+        SELECT t.TimeStamp as TimeStamp, {select}, n.Command
+        FROM {table_name} n
+        JOIN TraceHost h ON n.HOST_REF = h.HOST_ID
+        JOIN TimeLine t ON n.TL_REF = t.TL_ID
+        WHERE h.HostName = '{{host_name}}'""".format(select=', '.join([f"n.{c} as {c}" for c in self.sections]),
+                                                     table_name=self._table.name)
+
+    def y_axes(self, data: [Iterable[Iterable]]) -> Iterable[Any]:
+        return [s.replace(self.title, '') for s in self.sections]
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join(self.sections)}"
+
+    def generate_chart_data(self, query_results: Iterable[Iterable], extension=None) -> \
+            Iterable[Tuple[str, Iterable, Iterable, Iterable[Iterable]]]:
+
+        data_series = {}
+        for row in query_results:
+            if row[-1] not in data_series.keys():
+                data_series.update({row[-1]: []})
+            data_series[row[-1]].append(row[:-1])
+        result = []
+        for cmd, row in data_series.items():
+            result.append(super().generate_chart_data(row, cmd)[0])
+        return result
 
 
 class TimeParser(plugins.Parser):
@@ -60,8 +198,8 @@ class TimeSSHCommand(plugins.SSHLibraryCommand):
 
 
 class Time(plugins.PlugInAPI):
-    def __init__(self, parameters, data_handler, **options):
-        plugins.PlugInAPI.__init__(self, parameters, data_handler, **options)
+    def __init__(self, parameters, data_handler, *args, **options):
+        plugins.PlugInAPI.__init__(self, parameters, data_handler, *args, **options)
         self._prefix = f"{self.__class__.__name__}_item:"
 
         self._time_cmd = options.get('time_cmd', DEFAULT_TIME_COMMAND)
@@ -130,3 +268,9 @@ def cache_output(output: str):
                                         lines_ref)
 
     return effective_output_ref
+
+
+__all__ = [
+    Time.__name__,
+    __doc__
+]
