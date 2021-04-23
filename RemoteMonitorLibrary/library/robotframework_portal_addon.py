f81ac3a2979238ed4055938bdcb71e7b50a8067a
doguz2509 <vWMA25Y6Vmc4UJSW4vUB>
2021-04-23 23:03:17 +0300

    atop_plugin.py: fix for os names; version.py - forward

--- RemoteMonitorLibrary/library/robotframework_portal_addon.py
+++ RemoteMonitorLibrary/library/robotframework_portal_addon.py
@@ -2,13 +2,36 @@ import os
 
 from RemoteMonitorLibrary.utils import Logger
 
+__doc__ = """ === agent-PortalRobotframework === 
+
+    Add to CLI
+    
+     --listener
+     robotframework_reportportal.listener
+     --variable
+     RP_UUID:077ee5d9-b6ad-47c7-9a5c-9f29a6826a25
+     --variable
+     RP_ENDPOINT:http://192.168.25.5:8080
+     --variable
+     RP_LAUNCH:RemoteMonitor
+     --variable
+     RP_PROJECT:dmitry_oguz_personal
+     --variable
+     ENV:TIRAMISU
+     --variable
+     PLATFORM:CUSTOM_REMOTE_CHROME
+     --variable
+     x_api_version:1.4.0
+     
+     """
+
 try:
     from robotframework_reportportal import logger as portal_logger
     from robotframework_reportportal.exception import RobotServiceException
     PORTAL = True
     Logger().info(f"RobotFramework portal available")
 except (ImportError, ValueError):
-    Logger().warn(f"RobotFramework portal not available")
+    Logger().warning(f"RobotFramework portal not available")
     PORTAL = False
 
 
